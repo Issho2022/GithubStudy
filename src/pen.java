fc7923d98469c868270690698eab61e19106e368
Issho <1069159061@qq.com>
2022-02-09 14:34:42 +0800

    hotfix commit1

--- src/pen.java
+++ src/pen.java
@@ -1,5 +1,8 @@
 public class pen {
     public static void main(String[] args){
         System.out.println("asdasd");
+        System.out.println("asdasd");
+        System.out.println("asdasd");
+        System.out.println("asdasd");
     }
 }
